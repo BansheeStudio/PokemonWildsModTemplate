ae3df110ace4c27bc826b9879a2693ff77941f5b
BansheeStudio <113316414+BansheeStudio@users.noreply.github.com>
2023-07-12 20:11:49 +1000

    Main

---
+++ ModTemplate/mods/pokemon/pokemon_overworld_adjustments.asm
@@ -0,0 +1,1009 @@
+Golduck:
+ db i, 303
+ db found, true
+ db flip, true
+
+Hatenna:
+ db i, 304
+ db found, true
+ db flip, false
+
+Kabuto:
+ db i, 305
+ db found, true
+ db flip, false
+
+Gyamask:
+ db i, 306
+ db found, true
+ db flip, false
+
+Noivern:
+ db i, 307
+ db found, true
+ db flip, false
+
+Omanyte:
+ db i, 308
+ db found, true
+ db flip, true
+
+Omastar:
+ db i, 309
+ db found, true
+ db flip, true
+
+Psyduck:
+ db i, 310
+ db found, true
+ db flip, true
+
+Wailmer:
+ db i, 311
+ db found, true
+ db flip, false
+
+Poochyena:
+ db i, 312
+ db found, true
+ db flip, false
+
+Mightyena:
+ db i, 313
+ db found, true
+ db flip, true
+
+Wingull:
+ db i, 314
+ db found, true
+ db flip, true
+
+Pelipper:
+ db i, 315
+ db found, true
+ db flip, true
+
+Snover:
+ db i, 316
+ db found, true
+ db flip, true
+
+Mimikyu:
+ db i, 317
+ db found, true
+ db flip, true
+
+Corphish:
+ db i, 318
+ db found, true
+ db flip, true
+
+Crawdaunt:
+ db i, 319
+ db found, true
+ db flip, true
+
+Litwick:
+ db i, 320
+ db found, true
+ db flip, false
+
+Lampent:
+ db i, 321
+ db found, true
+ db flip, false
+
+Chandelure:
+ db i, 322
+ db found, true
+ db flip, false
+
+Dwebble:
+ db i, 323
+ db found, true
+ db flip, false
+
+Crustle:
+ db i, 324
+ db found, true
+ db flip, false
+
+Goomy:
+ db i, 325
+ db found, true
+ db flip, false
+
+Wailord:
+ db i, 326
+ db found, true
+ db flip, false
+
+Sandshrew:
+ db i, 327
+ db found, true
+ db flip, false
+
+Regidrago:
+ db i, 328
+ db found, true
+ db flip, true
+
+Regieleki:
+ db i, 329
+ db found, true
+ db flip, true
+
+Regice:
+ db i, 330
+ db found, true
+ db flip, false
+
+Regirock:
+ db i, 331
+ db found, true
+ db flip, false
+
+Registeel:
+ db i, 332
+ db found, true
+ db flip, false
+
+Regigigas:
+ db i, 333
+ db found, true
+ db flip, true
+
+Darmanitan:
+ db i, 334
+ db found, true
+ db flip, false
+
+Darmanitanzen:
+ db i, 335
+ db found, true
+ db flip, false
+
+Cacturne:
+ db i, 336
+ db found, true
+ db flip, true
+
+Elgyem:
+ db i, 339
+ db found, true
+ db flip, false
+
+Hippopotas:
+ db i, 340
+ db found, true
+ db flip, false
+
+Cutiefly:
+ db i, 341
+ db found, true
+ db flip, false
+
+Ribombee:
+ db i, 342
+ db found, true
+ db flip, false
+
+Sigilyph:
+ db i, 343
+ db found, true
+ db flip, false
+
+Sandile:
+ db i, 344
+ db found, true
+ db flip, false
+
+Krokorok:
+ db i, 345
+ db found, true
+ db flip, false
+
+Krookodile:
+ db i, 346
+ db found, true
+ db flip, false
+
+Nosepass:
+ db i, 347
+ db found, true
+ db flip, false
+
+Vespiquen:
+ db i, 348
+ db found, true
+ db flip, false
+
+Beheeyem:
+ db i, 349
+ db found, true
+ db flip, false
+
+Zigzagoon:
+ db i, 350
+ db found, true
+ db flip, false
+
+Volcarona:
+ db i, 351
+ db found, true
+ db flip, false
+
+Larvesta:
+ db i, 352
+ db found, true
+ db flip, false
+
+Abomasnow:
+ db i, 353
+ db found, true
+ db flip, true
+
+Linoone:
+ db i, 354
+ db found, true
+ db flip, false
+
+Wooper:
+ db i, 355
+ db found, true
+ db flip, true
+
+Weedle:
+ db i, 356
+ db found, true
+ db flip, false
+
+Kakuna:
+ db i, 357
+ db found, true
+ db flip, false
+
+Beedrill:
+ db i, 358
+ db found, true
+ db flip, false
+
+Rattata:
+ db i, 359
+ db found, true
+ db flip, true
+
+Raticate:
+ db i, 360
+ db found, true
+ db flip, true
+
+Grimer:
+ db i, 361
+ db found, true
+ db flip, false
+
+Muk:
+ db i, 362
+ db found, true
+ db flip, false
+
+Miltank:
+ db i, 363
+ db found, true
+ db flip, true
+
+Pinsir:
+ db i, 364
+ db found, true
+ db flip, true
+
+Diglett:
+ db i, 365
+ db found, true
+ db flip, false
+
+Dugtrio:
+ db i, 366
+ db found, true
+ db flip, false
+
+Voltorb:
+ db i, 367
+ db found, true
+ db flip, false
+
+Sudowoodo:
+ db i, 368
+ db found, true
+ db flip, true
+
+Mrmime:
+ db i, 369
+ db found, true
+ db flip, true
+
+Wooloo:
+ db i, 370
+ db found, true
+ db flip, true
+
+Smoochum:
+ db i, 371
+ db found, true
+ db flip, true
+
+Mantine:
+ db i, 372
+ db found, true
+ db flip, false
+
+Mantyke:
+ db i, 373
+ db found, true
+ db flip, false
+
+Horsea:
+ db i, 374
+ db found, true
+ db flip, false
+
+Probopass:
+ db i, 375
+ db found, true
+ db flip, false
+
+Seadra:
+ db i, 376
+ db found, true
+ db flip, false
+
+Kingdra:
+ db i, 377
+ db found, true
+ db flip, false
+
+Ekans:
+ db i, 378
+ db found, true
+ db flip, false
+
+Arbok:
+ db i, 379
+ db found, true
+ db flip, false
+
+Trevenant:
+ db i, 380
+ db found, true
+ db flip, true
+
+Sandslash:
+ db i, 381
+ db found, true
+ db flip, true
+
+Lapras:
+ db i, 382
+ db found, true
+ db flip, false
+
+Dragonite:
+ db i, 383
+ db found, true
+ db flip, false
+
+Dragonair:
+ db i, 384
+ db found, true
+ db flip, true
+
+Dratini:
+ db i, 385
+ db found, true
+ db flip, true
+
+Hoothoot:
+ db i, 386
+ db found, true
+ db flip, false
+
+Ledyba:
+ db i, 387
+ db found, true
+ db flip, false
+
+Sharpedo:
+ db i, 388
+ db found, true
+ db flip, false
+
+Luvdisc:
+ db i, 389
+ db found, true
+ db flip, false
+
+Duraludon:
+ db i, 390
+ db found, true
+ db flip, true
+
+Snom:
+ db i, 391
+ db found, true
+ db flip, false
+
+Klefki:
+ db i, 392
+ db found, true
+ db flip, false
+
+Palossand:
+ db i, 393
+ db found, true
+ db flip, false
+
+Dedenne:
+ db i, 394
+ db found, true
+ db flip, true
+
+Pumpkaboo:
+ db i, 395
+ db found, true
+ db flip, false
+
+Frosmoth:
+ db i, 396
+ db found, true
+ db flip, false
+
+Honedge:
+ db i, 397
+ db found, true
+ db flip, false
+
+Carbink:
+ db i, 398
+ db found, true
+ db flip, false
+
+Diancie:
+ db i, 399
+ db found, true
+ db flip, false
+
+Aipom:
+ db i, 400
+ db found, true
+ db flip, true
+
+Ambipom:
+ db i, 401
+ db found, true
+ db flip, true
+
+Hoppip:
+ db i, 402
+ db found, true
+ db flip, false
+
+Jynx:
+ db i, 403
+ db found, true
+ db flip, true
+
+Azurill:
+ db i, 404
+ db found, true
+ db flip, false
+
+Bidoof:
+ db i, 405
+ db found, true
+ db flip, false
+
+Dodrio:
+ db i, 406
+ db found, true
+ db flip, false
+
+Doduo:
+ db i, 407
+ db found, true
+ db flip, false
+
+Entei:
+ db i, 408
+ db found, true
+ db flip, false
+
+Gloom:
+ db i, 409
+ db found, true
+ db flip, false
+
+Gourgeist:
+ db i, 410
+ db found, true
+ db flip, false
+
+Vileplume:
+ db i, 411
+ db found, true
+ db flip, false
+
+Starmie:
+ db i, 412
+ db found, true
+ db flip, false
+
+Quagsire:
+ db i, 413
+ db found, true
+ db flip, false
+
+Phantump:
+ db i, 414
+ db found, true
+ db flip, false
+
+Oddish:
+ db i, 415
+ db found, true
+ db flip, true
+
+Staryu:
+ db i, 416
+ db found, true
+ db flip, false
+
+Skiploom:
+ db i, 417
+ db found, true
+ db flip, false
+
+Jumpluff:
+ db i, 418
+ db found, true
+ db flip, false
+
+Poliwag:
+ db i, 419
+ db found, true
+ db flip, false
+
+Sunflora:
+ db i, 420
+ db found, true
+ db flip, false
+
+Politoed:
+ db i, 421
+ db found, true
+ db flip, false
+
+Poliwhirl:
+ db i, 422
+ db found, true
+ db flip, false
+
+Poliwrath:
+ db i, 423
+ db found, true
+ db flip, false
+
+Raikou:
+ db i, 424
+ db found, true
+ db flip, false
+
+Suicune:
+ db i, 425
+ db found, true
+ db flip, false
+
+Sunkern:
+ db i, 426
+ db found, true
+ db flip, false
+
+Bibarel:
+ db i, 427
+ db found, true
+ db flip, true
+
+Buizel:
+ db i, 428
+ db found, true
+ db flip, true
+
+Floatzel:
+ db i, 429
+ db found, true
+ db flip, true
+
+Hatterene:
+ db i, 430
+ db found, true
+ db flip, false
+
+Minun:
+ db i, 431
+ db found, true
+ db flip, true
+
+Noibat:
+ db i, 432
+ db found, true
+ db flip, false
+
+Pikipek:
+ db i, 433
+ db found, true
+ db flip, false
+
+Plusle:
+ db i, 434
+ db found, true
+ db flip, true
+
+Toucannon:
+ db i, 435
+ db found, true
+ db flip, false
+
+Trumbeak:
+ db i, 436
+ db found, true
+ db flip, false
+
+Combee:
+ db i, 437
+ db found, true
+ db flip, false
+
+Maractus:
+ db i, 438
+ db found, true
+ db flip, false
+
+Darumaka:
+ db i, 439
+ db found, true
+ db flip, false
+
+Sandygast:
+ db i, 440
+ db found, true
+ db flip, false
+
+Applin:
+ db i, 441
+ db found, true
+ db flip, false
+
+Appletun:
+ db i, 442
+ db found, true
+ db flip, true
+
+Murkrow:
+ db i, 443
+ db found, true
+ db flip, false
+
+Honchkrow:
+ db i, 444
+ db found, true
+ db flip, false
+
+Remoraid:
+ db i, 445
+ db found, true
+ db flip, false
+
+Octillery:
+ db i, 446
+ db found, true
+ db flip, false
+
+Stonjourner:
+ db i, 447
+ db found, true
+ db flip, false
+
+Delibird:
+ db i, 448
+ db found, true
+ db flip, false
+
+Flapple:
+ db i, 449
+ db found, true
+ db flip, false
+
+Qwilfish:
+ db i, 450
+ db found, true
+ db flip, false
+
+Shuckle:
+ db i, 451
+ db found, true
+ db flip, false
+
+Marowak:
+ db i, 452
+ db found, true
+ db flip, false
+
+Amarowak:
+ db i, 453
+ db found, true
+ db flip, false
+
+Primeape:
+ db i, 454
+ db found, true
+ db flip, false
+
+Phione:
+ db i, 455
+ db found, true
+ db flip, false
+
+Manaphy:
+ db i, 456
+ db found, true
+ db flip, false
+
+Happiny:
+ db i, 457
+ db found, true
+ db flip, false
+
+Heracross:
+ db i, 458
+ db found, true
+ db flip, false
+
+Bellossom:
+ db i, 459
+ db found, true
+ db flip, false
+
+Drowzee:
+ db i, 460
+ db found, true
+ db flip, false
+
+Dusknoir:
+ db i, 461
+ db found, true
+ db flip, true
+
+Carvanha:
+ db i, 462
+ db found, true
+ db flip, false
+
+Swirlix:
+ db i, 463
+ db found, true
+ db flip, false
+
+Electrike:
+ db i, 464
+ db found, true
+ db flip, false
+
+Starly:
+ db i, 465
+ db found, true
+ db flip, false
+
+Staravia:
+ db i, 466
+ db found, true
+ db flip, false
+
+Hydreigon:
+ db i, 467
+ db found, true
+ db flip, false
+
+Nidoran_F:
+ db i, 468
+ db found, true
+ db flip, true
+
+Nidorina:
+ db i, 469
+ db found, true
+ db flip, true
+
+Nidoqueen:
+ db i, 470
+ db found, true
+ db flip, true
+
+Tauros:
+ db i, 471
+ db found, true
+ db flip, true
+
+Skarmory:
+ db i, 472
+ db found, true
+ db flip, false
+
+Aerodactyl:
+ db i, 473
+ db found, true
+ db flip, false
+
+Nidoran_M:
+ db i, 474
+ db found, true
+ db flip, true
+
+Jirachi:
+ db i, 475
+ db found, true
+ db flip, false
+
+Cosmog:
+ db i, 476
+ db found, true
+ db flip, false
+
+Cosmoem:
+ db i, 477
+ db found, true
+ db flip, false
+
+Zweilous:
+ db i, 478
+ db found, true
+ db flip, true
+
+Deino:
+ db i, 479
+ db found, true
+ db flip, true
+
+Girafarig:
+ db i, 480
+ db found, true
+ db flip, false
+
+Cufant:
+ db i, 481
+ db found, true
+ db flip, false
+
+Pachirisu:
+ db i, 482
+ db found, true
+ db flip, false
+
+Milcery:
+ db i, 483
+ db found, true
+ db flip, false
+
+Cottonee:
+ db i, 484
+ db found, true
+ db flip, false
+
+Noctowl:
+ db i, 485
+ db found, true
+ db flip, false
+
+Nidoking:
+ db i, 486
+ db found, true
+ db flip, true
+
+Cubone:
+ db i, 487
+ db found, true
+ db flip, true
+
+Whimsicott:
+ db i, 488
+ db found, true
+ db flip, false
+
+Dhelmise:
+ db i, 489
+ db found, true
+ db flip, false
+
+Drilbur:
+ db i, 490
+ db found, true
+ db flip, true
+
+Excadrill:
+ db i, 491
+ db found, true
+ db flip, true
+
+Mime_Jr:
+ db i, 492
+ db found, true
+ db flip, false
+
+Pancham:
+ db i, 493
+ db found, true
+ db flip, false
+
+Golett:
+ db i, 494
+ db found, true
+ db flip, true
+
+Golurk:
+ db i, 495
+ db found, true
+ db flip, true
+
+Oshawott:
+ db i, 496
+ db found, true
+ db flip, false
+
+Pangoro:
+ db i, 497
+ db found, true
+ db flip, false
+
+Ledian:
+ db i, 498
+ db found, true
+ db flip, false
+
+Lilligant:
+ db i, 499
+ db found, true
+ db flip, false
+
+Petilil:
+ db i, 500
+ db found, true
+ db flip, true
+
+Munchlax:
+ db i, 501
+ db found, true
+ db flip, true
+
+Dewott:
+ db i, 502
+ db found, true
+ db flip, false
+
+Hattrem:
+ db i, 503
+ db found, true
+ db flip, true
+
+Clefairy:
+ db i, 504
+ db found, true
+ db flip, true
+
+Croagunk:
+ db i, 505
+ db found, true
+ db flip, true
+
+Cramorant:
+ db i, 506
+ db found, true
+ db flip, true
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # PokemonWildsModTemplate
 0.8.X
 this is a template i made ages ago for young modders that want to start modding wilds can use it has all the textures, audio, attack data you need to start making mods i hope this helps anyone that wants to start making mods for wilds
+
+Lead - PlushBanshee
+dump of game files and sorting them
+
+Contributor - earthoul
+made a cheatsheet of the easily accessible 225 moves on v0.8.9
